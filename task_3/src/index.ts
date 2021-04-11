f1878c4f517557b29228efffa43daffda19098fe
Evgeniy Holmanskih <57038491+holmanskih@users.noreply.github.com>
2021-04-11 10:21:14 +0300

    Update index.ts

--- task_3/src/index.ts
+++ task_3/src/index.ts
@@ -1,16 +1,15 @@
 
-interface CartItem <T> {  
-  item:<T>{
-    id: number;
-    title: string; 
-    variantId: T;
-  }
-  
+interface CardItem {
+  id: number;
+  title: string;
+  variantId?: number;
 }
-const addToCart = <T>(item: {id: number, title: string, variantId}) => {
+
+const addToCart = (item: CardItem) => {
   console.log(`Adding "${item.title}" to cart.`);
 }
 
 
 
 addToCart({id: 1, title: 'Concrete shoes'});
+addToCart({id: 1, title: 'Concrete shoes', variantId: 15});
